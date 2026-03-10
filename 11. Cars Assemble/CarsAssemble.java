9182dfff34783768ca02594edb35584e9d7a3464
Snigdho <snigdhotf@gmail.com>
2026-03-10 23:57:14 +0100

    added problem 11 draft

---
+++ 11. Cars Assemble/CarsAssemble.java
@@ -0,0 +1,8 @@
+// https://exercism.org/tracks/java/exercises/cars-assemble
+
+public class CarsAssemble {
+
+    public double productionRatePerHour(int speed) {}
+
+    public int workingItemsPerMinute(int speed) {}
+}
