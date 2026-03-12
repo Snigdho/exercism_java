8a56bb98ada92afc95dde7af47bbcb8bbfa968e7
Snigdho <snigdhotf@gmail.com>
2026-03-12 23:31:23 +0100

    added problem 12

---
+++ 12. Log Levels/LogLevels.java
@@ -0,0 +1,19 @@
+// https://exercism.org/tracks/java/exercises/log-levels
+
+public class LogLevels {
+
+    public static String message(String logLine) {
+        return logLine.split(": ")[1].trim();
+    }
+
+    public static String logLevel(String logLine) {
+        return logLine
+            .split(": ")[0].replace("[", "")
+            .replace("]", "")
+            .toLowerCase();
+    }
+
+    public static String reformat(String logLine) {
+        return message(logLine) + " (" + logLevel(logLine) + ")";
+    }
+}
