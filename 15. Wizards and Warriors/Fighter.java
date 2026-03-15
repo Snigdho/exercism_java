84e870e2ebc2d1a4a9dc1af143dd50d708a32c1d
Snigdho <snigdhotf@gmail.com>
2026-03-15 23:57:37 +0100

    added problem 15 draft

---
+++ 15. Wizards and Warriors/Fighter.java
@@ -0,0 +1,54 @@
+class Fighter {
+
+    boolean isVulnerable() {
+        return true;
+    }
+
+    int getDamagePoints(Fighter fighter) {
+        return 1;
+    }
+}
+
+class Warrior extends Fighter {
+
+    public String toString() {
+        return "Fighter is a Warrior";
+    }
+
+    @Override
+    boolean isVulnerable() {
+        return false;
+    }
+
+    @Override
+    int getDamagePoints(Fighter fighter) {
+        return fighter.isVulnerable() ? 10 : 6;
+    }
+}
+
+class Wizard extends Fighter {
+
+    private boolean hasSpellPrepared;
+
+    public void Fighter() {
+        this.hasSpellPrepared = false;
+    }
+
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
+
+    public void prepareSpell() {
+        this.hasSpellPrepared = true;
+    }
+
+    @Override
+    public boolean isVulnerable() {
+        return !this.hasSpellPrepared;
+    }
+
+    @Override
+    public int getDamagePoints(Fighter fighter) {
+        return fighter.isVulnerable() ? 12 : 3;
+    }
+}
