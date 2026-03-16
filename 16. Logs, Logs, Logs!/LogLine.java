433b8422af1895fa573961ded054ad6f1f231b1f
Snigdho <snigdhotf@gmail.com>
2026-03-16 23:59:33 +0100

    added problem 16

---
+++ 16. Logs, Logs, Logs!/LogLine.java
@@ -0,0 +1,59 @@
+// https://exercism.org/tracks/java/exercises/logs-logs-logs
+
+public class LogLine {
+
+    private final String line;
+
+    public LogLine(String logLine) {
+        this.line = logLine;
+    }
+
+    public LogLevel getLogLevel() {
+        String code = line.substring(1, 4);
+
+        switch (code) {
+            case "TRC":
+                return LogLevel.TRACE;
+            case "DBG":
+                return LogLevel.DEBUG;
+            case "INF":
+                return LogLevel.INFO;
+            case "WRN":
+                return LogLevel.WARNING;
+            case "ERR":
+                return LogLevel.ERROR;
+            case "FTL":
+                return LogLevel.FATAL;
+            default:
+                return LogLevel.UNKNOWN;
+        }
+    }
+
+    public String getOutputForShortLog() {
+        LogLevel level = this.getLogLevel();
+        String message = line.substring(line.indexOf(":") + 2);
+        int encoded = 0;
+
+        switch (level) {
+            case TRACE:
+                encoded = 1;
+                break;
+            case DEBUG:
+                encoded = 2;
+                break;
+            case INFO:
+                encoded = 4;
+                break;
+            case WARNING:
+                encoded = 5;
+                break;
+            case ERROR:
+                encoded = 6;
+                break;
+            case FATAL:
+                encoded = 42;
+                break;
+        }
+        return encoded + ":" + message;
+    }
+}
