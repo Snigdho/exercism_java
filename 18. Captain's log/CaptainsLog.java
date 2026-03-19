8f45866a548b9213d867bfd32599b62e8c096766
Snigdho <snigdhotf@gmail.com>
2026-03-19 08:21:26 +0100

    added problem 18

---
+++ 18. Captain's log/CaptainsLog.java
@@ -0,0 +1,37 @@
+// https://exercism.org/tracks/java/exercises/captains-log
+
+import java.util.Random;
+
+class CaptainsLog {
+
+    private static final char[] PLANET_CLASSES = new char[] {
+        'D',
+        'H',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'R',
+        'T',
+        'Y',
+    };
+
+    private Random random;
+
+    CaptainsLog(Random random) {
+        this.random = random;
+    }
+
+    char randomPlanetClass() {
+        return PLANET_CLASSES[random.nextInt(PLANET_CLASSES.length)];
+    }
+
+    String randomShipRegistryNumber() {
+        return "NCC-" + (1000 + random.nextInt(9000));
+    }
+
+    double randomStardate() {
+        return 41000.0 + (42000.0 - 41000.0) * random.nextDouble();
+    }
+}
