39c7051474c5a94d68c26e27e4d71ed649400b02
Snigdho <snigdhotf@gmail.com>
2026-03-22 01:26:18 +0100

    optimized problem 20

--- 20. Wizards and Warriors 2/GameMaster.java
+++ 20. Wizards and Warriors 2/GameMaster.java
@@ -3,19 +3,24 @@
 public class GameMaster {
 
     public String describe(Character c) {
-        return String.format(
-            "You're a level %d %s with %d hit points.",
-            c.getLevel(),
-            c.getCharacterClass(),
-            c.getHitPoints()
+        return (
+            "You're a level " +
+            c.getLevel() +
+            " " +
+            c.getCharacterClass() +
+            " with " +
+            c.getHitPoints() +
+            " hit points."
         );
     }
 
     public String describe(Destination d) {
-        return String.format(
-            "You've arrived at %s, which has %d inhabitants.",
-            d.getName(),
-            d.getInhabitants()
+        return (
+            "You've arrived at " +
+            d.getName() +
+            ", which has " +
+            d.getInhabitants() +
+            " inhabitants."
         );
     }
 
