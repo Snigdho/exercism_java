1d0afb6b107bc6331ba6573e50e6a2e5fa68c27e
Snigdho <snigdhotf@gmail.com>
2026-03-23 23:48:45 +0100

    optimized problem 21

---
+++ 21. Secrets/Secrets.java
@@ -0,0 +1,20 @@
+// https://exercism.org/tracks/java/exercises/secrets
+
+public class Secrets {
+
+    public static int shiftBack(int value, int amount) {
+        return value >>> amount;
+    }
+
+    public static int setBits(int value, int mask) {
+        return value | mask;
+    }
+
+    public static int flipBits(int value, int mask) {
+        return value ^ mask;
+    }
+
+    public static int clearBits(int value, int mask) {
+        return value & ~mask;
+    }
+}
