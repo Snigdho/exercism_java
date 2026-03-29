a5d45b6a6391b6d31c562719894c28aeff59c4a2
Snigdho <snigdhotf@gmail.com>
2026-03-29 11:57:55 +0200

    added problem 25

---
+++ 25. Reverse String/ReverseString.java
@@ -0,0 +1,8 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+        if (inputString == null) return null; // handles null first
+        StringBuilder reversed = new StringBuilder(inputString);
+        return reversed.reverse().toString();
+    }
+}
