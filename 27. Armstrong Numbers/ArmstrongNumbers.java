42dac948895c4fcb8543d3de42539fa287b38a22
Snigdho <snigdhotf@gmail.com>
2026-03-31 23:42:02 +0200

    added problem 27

---
+++ 27. Armstrong Numbers/ArmstrongNumbers.java
@@ -0,0 +1,15 @@
+class ArmstrongNumbers {
+
+    boolean isArmstrongNumber(int numberToCheck) {
+        String numberStr = String.valueOf(numberToCheck);
+        int numDigits = numberStr.length();
+
+        int sum = 0;
+        for (char digitChar : numberStr.toCharArray()) {
+            int digit = digitChar - '0';
+            sum += Math.pow(digit, numDigits);
+        }
+
+        return sum == numberToCheck;
+    }
+}
