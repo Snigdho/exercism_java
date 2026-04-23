b602ddbbe8192baad95d8489d29c2104faff2b12
Snigdho <snigdhotf@gmail.com>
2026-04-23 23:51:28 +0200

    added problem 38

---
+++ 38. Difference of Squares/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,17 @@
+// https://exercism.org/tracks/java/exercises/difference-of-squares
+
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int input) {
+        int sum = (input * (input + 1)) / 2;
+        return sum * sum;
+    }
+
+    int computeSumOfSquaresTo(int input) {
+        return (input * (input + 1) * (2 * input + 1)) / 6;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+}
